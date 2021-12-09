173d87ed731241007fa0a0ae447d0519d14061be
semmet95 <singhamitch@gmail.com>
2021-12-09 23:43:01 +0530

    adds UI components

--- src/App.js
+++ src/App.js
@@ -6,12 +6,19 @@ import lottery from "./lottery";
 
 class App extends React.Component {
 
-  state = { manager: '' };
+  state = {
+    manager: '',
+    players: [],
+    balance: '',
+    value: ''
+  };
 
   async componentDidMount() {
     const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
 
-    this.setState({ manager });
+    this.setState({ manager, players, balance });
   }
 
   render() {
@@ -19,7 +26,25 @@ class App extends React.Component {
     return (
       <div>
         <h2>Lottery Contract</h2>
-        <p>This contract is managed by {this.state.manager}</p>
+        <p>
+          This contract is managed by {this.state.manager} <br />
+          Number of competing players: {this.state.players.length} <br />
+          Lottery Balance: {web3.utils.fromWei(this.state.balance, 'ether')} ether
+        </p>
+
+        <hr />
+
+        <form onSubmit={this.onSubmit}>
+          <h4>Wanna give it a shot?</h4>
+          <div>
+            <label>Amount of ether to enter: </label>
+            <input
+              value = {this.state.value}
+              onChange={event => this.setState({ value: event.target.value })}
+            />
+          </div>
+          <button>Enter</button>
+        </form>
       </div>
     );
   }
